adae776c189f24cbee0f50b0cd93269976688759
juandavidg0mez <nihlgomez@gamil.com>
2024-08-30 23:46:35 -0500

    Creacion de entiedad y ambebbleas suerveys

--- project/src/main/java/com/projectmain/project/domain/entity/Surveys.java
+++ project/src/main/java/com/projectmain/project/domain/entity/Surveys.java
@@ -1,5 +1,6 @@
 package com.projectmain.project.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,74 @@ public class Surveys {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition = "VARCHAR(20)")
+    private String compenenthtml;
+
+    @Column(columnDefinition = "VARCHAR(20)")
+    private String componentreact;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(columnDefinition = "TEXT")
+    private String name;
+
+    @Column
+    private Audit audit;
+
+    
+    public Surveys() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompenenthtml() {
+        return compenenthtml;
+    }
+
+    public void setCompenenthtml(String compenenthtml) {
+        this.compenenthtml = compenenthtml;
+    }
+
+    public String getComponentreact() {
+        return componentreact;
+    }
+
+    public void setComponentreact(String componentreact) {
+        this.componentreact = componentreact;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Audit getAudit() {
+        return audit;
+    }
+
+    public void setAudit(Audit audit) {
+        this.audit = audit;
+    }
     
 
 
